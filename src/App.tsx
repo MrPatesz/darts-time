eaeba9f08e1f500f8f523bb93b4cb06386f40412
MrPatesz <patesz1999@gmail.com>
2026-07-25 18:57:51 +0200

    screen wake lock on mobile

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,13 @@
+import { useEffect } from "react";
+
 export function App() {
+    useEffect(() => {
+        try {
+            void navigator.wakeLock.request("screen");
+            // @ts-expect-error
+            navigator.virtualKeyboard.overlaysContent = true;
+        } catch (_) {}
+    }, []);
+
     return <></>;
 }
